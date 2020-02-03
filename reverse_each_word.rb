60bb285819c0d09fd49c9403dc4b8d4b5f070f98
Ricky Rozier <rickydrozier@gmail.com>
2020-02-03 05:25:19 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,22 @@
+def reverse_each_word_with_each(array)
+
+  original_array = string.split(" ")
+  return_array = []
+  original_array.each do|array|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
+
+def reverse_each_word(string)
+  array = string.split(" ") #turn string into an array
+  test_array = []
+  array.collect do|string| #iterate over the array
+    test_array << string.reverse #reverse each word in the array
+  end
+  test_array.join(" ")
+end
+
+reverse_each_word("Hello there, and how are you?")
+
+  
